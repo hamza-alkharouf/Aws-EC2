17cdc9ea4b81023f45fbf26971d369de9d31fe9e
hamza-alkharouf <hamzaalkharouf5@gmail.com>
2022-05-18 02:18:15 +0300

    .

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.get("/mywebapp", (req, res) => {
+  res.send("<h1>It's working 🤗</h1>");
+});
+
+app.get("/", (req, res) => {
+  res.send("<h1>Go to .../mywebapp</h1>");
+});
+
+const port = 8080;
+app.listen(port, () => console.log(`Listening on port ${port}`));
